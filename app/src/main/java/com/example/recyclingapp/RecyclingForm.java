d5f902da43e0652f2def90cde23bd415420bb8f4
Eva Sigma <eva13sigma@gmail.com>
2024-04-18 15:31:53 +0300

    Eva's form

---
+++ app/src/main/java/com/example/recyclingapp/RecyclingForm.java
@@ -0,0 +1,14 @@
+package com.example.recyclingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecyclingForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycling_form);
+    }
+}
