7b7b425a083d82ca85638d6ceae96e510e06a833
Florian Cenko <tsenkofloris@gmail.com>
2024-05-15 21:01:34 +0300

    Improvement of Flori's,Eva's and Panteli's fragments

--- app/src/main/java/com/example/recyclingapp/MainActivity.java
+++ app/src/main/java/com/example/recyclingapp/MainActivity.java
@@ -13,19 +13,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycling_form);
+        setContentView(R.layout.activity_main_page);
 
-       // rg = findViewById(R.id.rg_user_admin);
-    //    RadioButton userButton = new RadioButton(this);
-      //  userButton.setId(View.generateViewId());
-      //  userButton.setText("User");
-
-      //  RadioButton adminButton = new RadioButton(this);
-      //  adminButton.setId(View.generateViewId());
-      //  adminButton.setText("Administrator");
-
-    //    rg.addView(userButton);
-      //  rg.addView(adminButton);
+//        rg = findViewById(R.id.rg_user_admin);
+//        RadioButton userButton = new RadioButton(this);
+//        userButton.setId(View.generateViewId());
+//        userButton.setText("User");
+//        userButton.setTextColor(getColor(android.R.color.white));
+//        userButton.setTextSize(20);
+//
+//        RadioButton adminButton = new RadioButton(this);
+//        adminButton.setId(View.generateViewId());
+//        adminButton.setText("Administrator");
+//        adminButton.setTextColor(getColor(android.R.color.white));
+//        adminButton.setTextSize(20);
+//        rg.addView(userButton);
+//        rg.addView(adminButton);
 
     }
     public void approveRecordAndRewardPoints(View v){
