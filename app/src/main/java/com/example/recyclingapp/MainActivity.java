abede8180c74d76c0b5d898b9e16b27a9e035397
Florian Cenko <tsenkofloris@gmail.com>
2024-05-17 23:10:40 +0300

    Pantelis Frame

--- app/src/main/java/com/example/recyclingapp/MainActivity.java
+++ app/src/main/java/com/example/recyclingapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+        setContentView(R.layout.activity_recycling_sign_up);
 
 //        rg = findViewById(R.id.rg_user_admin);
 //        RadioButton userButton = new RadioButton(this);
