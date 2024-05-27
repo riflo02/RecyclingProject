deba54fc9e17978c1a9344f2dd3c6ad22dd0c7c6
Florian Cenko <tsenkofloris@gmail.com>
2024-05-27 21:50:26 +0300

    Top Statistics

---
+++ app/src/main/java/com/example/recyclingapp/AdminTopStats.java
@@ -0,0 +1,19 @@
+package com.example.recyclingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AdminTopStats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_top_stats);
+
+
+    }
+}
