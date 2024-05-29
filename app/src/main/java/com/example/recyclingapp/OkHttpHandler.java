d5a1a9ccd55cc23006780c6d7a307e9114c3e3fd
Florian Cenko <tsenkofloris@gmail.com>
2024-05-29 20:57:17 +0300

    Fetching

--- app/src/main/java/com/example/recyclingapp/OkHttpHandler.java
+++ app/src/main/java/com/example/recyclingapp/OkHttpHandler.java
@@ -2,50 +2,17 @@ package com.example.recyclingapp;
 
 import android.os.StrictMode;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-
-//public class OkHttpHandler {
-//
-//    public OkHttpHandler() {
-//        StrictMode.ThreadPolicy policy = new
-//                StrictMode.ThreadPolicy.Builder().permitAll().build();
-//        StrictMode.setThreadPolicy(policy);
-//    }
-//
-    /*
-    ArrayList<User> populateDropDown(String url) throws Exception {
-        ArrayList<User> cbList = new ArrayList<>();
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-        RequestBody body = RequestBody.create("",
-                MediaType.parse("text/plain"));
-        Request request = new Request.Builder().url(url).method("POST",
-                body).build();
-        Response response = client.newCall(request).execute();
-        String data = response.body().string();
-        //System.out.println("My Response: " + data);
-        try {
-
-            JSONObject json = new JSONObject(data);
-            Iterator<String> keys = json.keys();
-            while (keys.hasNext()) {
-                 String brand = keys.next();
-                String models = json.get(brand).toString();
-                  cbList.add(new User(, ));
-             }
-        } catch(JSONException e)
-    {
-        e.printStackTrace();
-    }
-        return cbList;
-    }
-
-     */
-
-//
 
     public class OkHttpHandler {
 
@@ -63,5 +30,32 @@ import okhttp3.Response;
                     body).build();
             Response response = client.newCall(request).execute();
             System.out.println("My Response: " + response.body().string());
+        }
+
+        ArrayList<String[]> fetching(String url) throws Exception {
+            ArrayList<String[]> uList = new ArrayList<>();
+            OkHttpClient client = new OkHttpClient().newBuilder().build();
+            RequestBody body = RequestBody.create("",
+                    MediaType.parse("text/plain"));
+            Request request = new Request.Builder().url(url).method("POST",
+                    body).build();
+            Response response = client.newCall(request).execute();
+            String data = response.body().string();
+            //System.out.println("My Response: " + data);
+            try {
+
+                JSONObject json = new JSONObject(data);
+                Iterator<String> keys = json.keys();
+                while (keys.hasNext()) {
+                    String username = keys.next();
+                    String password = json.get(username).toString();
+                    String res[] = {username,password};
+                    uList.add(res);
+                }
+            } catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+            return uList;
         }
     }
--- app/src/main/java/com/example/recyclingapp/SignInActivity.java
+++ app/src/main/java/com/example/recyclingapp/SignInActivity.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class SignInActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
+public class SignInActivity extends AppCompatActivity {
+ String ip = "192.168.56.1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +36,26 @@ public class SignInActivity extends AppCompatActivity {
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignInActivity.this, UserMainPageActivity.class);
-                startActivity(intent);
+                ArrayList<String[]> uList = new ArrayList<>();
+                String url= "http://"+ip+"/fetching.php";
+                try {
+                    OkHttpHandler okHttpHandler = new OkHttpHandler();
+                    uList = okHttpHandler.fetching(url);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                EditText usernameInput = findViewById(R.id.uname_input);
+                String username = usernameInput.getText().toString();
+                EditText passwordInput = findViewById(R.id.password);
+                String password = passwordInput.getText().toString();
+                for(int i=0;i<uList.size();i++){
+                    if(uList.get(i)[0].equals(username) && uList.get(i)[1].equals(password)){
+                        Intent intent = new Intent(SignInActivity.this, UserMainPageActivity.class);
+                        startActivity(intent);
+                        break;
+                    }
+                }
+
             }
         });
     }
